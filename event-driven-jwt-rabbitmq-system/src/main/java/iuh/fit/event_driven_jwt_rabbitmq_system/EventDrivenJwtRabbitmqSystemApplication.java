ede471a3e102f59c03d13976c63e2e05f127d542
MinhDat1312 <nguyenthangdat84@gmail.com>
2026-01-15 13:49:59 +0700

    complete project lab 01

---
+++ event-driven-jwt-rabbitmq-system/src/main/java/iuh/fit/event_driven_jwt_rabbitmq_system/EventDrivenJwtRabbitmqSystemApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.event_driven_jwt_rabbitmq_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenJwtRabbitmqSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenJwtRabbitmqSystemApplication.class, args);
+	}
+
+}
